63ec85a292a936d86719a9aed8ab3a6bdfdd0da4
Luis Angel <angelfgdeveloper@gmail.com>
2022-10-09 21:10:39 -0500

    Init project webapi

---
+++ Controllers/WeatherForecastController.cs
@@ -0,0 +1,58 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace webapi.Controllers;
+
+[ApiController]
+[Route("api/[controller]")] // Generar ruta
+public class WeatherForecastController : ControllerBase
+{
+  private static readonly string[] Summaries = new[]
+  {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+  private readonly ILogger<WeatherForecastController> _logger;
+
+  private static List<WeatherForecast> ListWeatherForecast = new List<WeatherForecast>();
+
+  public WeatherForecastController(ILogger<WeatherForecastController> logger)
+  {
+    _logger = logger;
+
+    if (ListWeatherForecast == null || !ListWeatherForecast.Any()) // Any() Sino tiene ningun dato en la lista
+    {
+      ListWeatherForecast =  Enumerable.Range(1, 5).Select(index => new WeatherForecast
+      {
+        Date = DateTime.Now.AddDays(index),
+        TemperatureC = Random.Shared.Next(-20, 55),
+        Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+      }).ToList();
+    }
+
+  }
+
+  [HttpGet(Name = "GetWeatherForecast")]
+  [Route("Get/weatherforecast")] // Agregar ruta de ruta en ruta
+  [Route("Get/weatherforecast2")] // https://localhost:7294/api/weatherforecast/get/weatherforecast2
+  [Route("[action]")] // Permite utilizar el nombre delmetodo Get => https://localhost:7294/api/weatherforecast/get
+  public IEnumerable<WeatherForecast> Get()
+  {
+   return ListWeatherForecast;
+  }
+
+  [HttpPost]
+  public IActionResult Post(WeatherForecast weatherForecast)
+  {
+    ListWeatherForecast.Add(weatherForecast);
+
+    return Ok();
+  }
+
+  [HttpDelete("{index}")]
+  public IActionResult Delete(int index)
+  {
+    ListWeatherForecast.RemoveAt(index);
+
+    return Ok();
+  }
+}
---
+++ README.md
@@ -0,0 +1,19 @@
+# WebAPI
+* dotnet new => Para ver las aplicaciones mas utilizas
+* dotnet new --list => Ver la lista de todas las aplicaciones
+* dotnet new webapi => Es el que se utilizara para hacer una API REST
+
+## Funcionamiento de una API en .NET
+- dotnet run => Ejecutar proyecto
+
+Ruta por default: ```https://localhost:7294/weatherforecast```
+
+## Patrón MVC
+- Model => Datos
+- Vista => Interfaz de usuario
+- Controlador => Logica
+
+## Patrón MXC (No tiene vista "interfaz de usuario" X)
+- Model => Datos
+- Controller => Logica
+
